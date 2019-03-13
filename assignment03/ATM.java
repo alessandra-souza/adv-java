92f771d3ff69ed9af0d068c750701cac42109d96
alessandra-souza <aleferreira@gmail.com>
2019-03-12 20:45:41 -0400

    Added assignment03

---
+++ assignment03/ATM.java
@@ -0,0 +1,36 @@
+
+public class ATM {
+
+	public static void main(String[] args) {
+		
+		// Write a test program that creates an Account with annual interest rate 1.5%,
+		// balance 1000, id 1122, and name George. Deposit $30, $40, and $50 to the
+		// account and withdraw $5, $4, and $2 from the account. Print an account summary
+		// that shows the account holder name, interest rate, balance, and all transactions.
+		
+		Account georgeAcc = new Account(1122, 1000, "George");	
+		Account.setAnnualInterestRate(0.015);
+		
+		// Transactions
+		georgeAcc.deposit(30.00);
+		georgeAcc.deposit(40.00);
+		georgeAcc.deposit(50.00);
+		georgeAcc.withdraw(5.00);
+		georgeAcc.withdraw(4.00);
+		georgeAcc.withdraw(2.00);
+		
+		// Print summary
+		System.out.println("***********************************************************************");
+		System.out.println("Information about " + georgeAcc.getName() + "'s account:");
+		System.out.println("***********************************************************************");
+		
+		for (Transaction t : georgeAcc.getTransactions()) {
+			System.out.println("Interest rate: " + georgeAcc.getAnnualInterestRate());
+			System.out.println("Balance: " + t.getBalance());
+			System.out.println("Transaction: " + t.getDescription() + " in " + t.getDate() + ".");
+		}	
+		
+	}
+
+}
+
---
+++ assignment03/Transaction.java
@@ -0,0 +1,64 @@
+import java.util.Date;
+
+public class Transaction {
+	
+	// Data fields
+	private Date date;
+	private char type;
+	private double amount;
+	private double balance;
+	private String description;
+	
+	// Constructors
+	public Transaction(){};
+	
+	// Construct a Transaction with the specified date, type, balance, and description
+	public Transaction(Date date, char type, double balance, String description) {
+		this.date = date;
+		this.type = type;
+		this.balance = balance;
+		this.description = description;
+	}
+	
+	// Getters and setters
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public char getType() {
+		return type;
+	}
+
+	public void setType(char type) {
+		this.type = type;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
